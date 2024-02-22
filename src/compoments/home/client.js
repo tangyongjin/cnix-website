670b0214711969cc10461ce6d287e809b71857ed
alex <tangyongjin@gmail.com>
2024-02-22 10:56:39 +0800

    tmp state

--- src/compoments/home/client.js
+++ src/compoments/home/client.js
@@ -1,215 +1,86 @@
-import React, {
+import React,{
   Component
 } from 'react';
 
-class Client extends  Component{
+class Client extends Component {
   render() {
 
-  // canvas.particles.js
+    // canvas.particles.js
     return (
-         <section>
-	 
-        
-          <header className="text-center mb-60"><h2>我们的客户</h2>
-          </header>
+      <section>
 
-           <div className="container">
-        <ul className="row clients-dotted list-inline">
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/aliyun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/jinshanyun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/baiduyun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/qingyun.png" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/baidusm.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/alibaba.png" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/tengxun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/aiqiyi.png" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-            <br/>
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/ucloud.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/leshi.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/sohu.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-             <br /><br />
-		 <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/tengxunyun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-            <br/>
-            <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/tiandixiangyun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huliangangwan.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shouduzaixian.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-           <br/>
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/kugou.png" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huliantong.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/guanghuanxinwang.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/dnion.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huaxiayaku.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/zhongxinwangluo.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/xisiyun.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
 
-            <a href="#">
-             <br/>
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/gongjian.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/youchi.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/zhonglianhudong.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/maiyafangyuan.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/wanglianguangtong.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/yunpu.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/fangzhengkuandai.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/yiankuandai.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-            <br/>
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/kunshiwangluo.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/hangshukuanwang.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/pingmei.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/weishijiaye.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/guangdianxinchuang.jpg" alt="client" />
-            </a>
-          </li>
-          <li className="col-md-3 col-sm-3 col-xs-6">
-            <a href="#">
-              <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/kdy.jpg" alt="client" />
-            </a>
-          </li>
-        </ul>
-      </div>
+        <header className="text-center mb-60"><h2>我们的客户</h2>
+        </header>
+
+        <div className="container">
+
+
+
+          <ul className="row clients-dotted list-inline">
+
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/aiqiyi.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/aliyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/aws.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/baiduyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/baishanyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/beikuankeji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/gehuayouxian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/guangdianxinchuang.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/guanghuanxinwang.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/guanglianshiji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/guanmaikeji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/hebeiguangdian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huatongyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huaweiyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huitianwangluo.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/huoshanyinqing.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/jingdongyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/jinshanyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/jiuyunwuxian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/kuaishou.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/lianyunshiji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/nanlingkeji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/pengboshidashuju.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/qingyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/qishangzaixian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/senhuayiteng.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shanghaicengfeng.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shanghaierliusan.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shanghaisizhan.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shenzhenzhenyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shijihulian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shoudouzaixian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/shouxinyungang.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/taipingyangdianxin.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/tengxunyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/tiandixiangyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/tianweixintong.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/ucloud.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/wangsukeji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/wangyi.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/wangyinhulian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/weishijiaye.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/xiaomi.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/xisiyun.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/xuanhuikeji.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/yunchuangxuntong.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/zhongguoyouxian.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/zhonglianyungang.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/zhongxinwangluo.jpg" alt="client" />  </li>
+            <li className="col-md-3 col-sm-3 col-xs-6">  <img className="img-fluid" src="/demo_files/images/0haokuan/custlogos/ziguangyun.jpg" alt="client" />  </li>
+
+          </ul>
+
+
+
+        </div>
 
 
 
 
-          </section>
-         )
-    }
+      </section>
+    )
+  }
 };
 export default Client;
